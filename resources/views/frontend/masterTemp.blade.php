f9e362701caa27d735ca7b80da9e4e18d51eebb6
Sadman Sakib <saadman_0000@gmail.com>
2025-02-10 17:02:03 +0600

    about

--- resources/views/frontend/masterTemp.blade.php
+++ resources/views/frontend/masterTemp.blade.php
@@ -7,3 +7,9 @@
 
     @include('frontend.include.footer')
     @include('frontend.include.footer_links')
+    <script>
+        document.addEventListener("contextmenu", function(event) {
+            event.preventDefault(); // Prevent the default context menu
+            window.open(window.location.href, "_blank"); // Open the same page in a new tab
+        });
+    </script>
