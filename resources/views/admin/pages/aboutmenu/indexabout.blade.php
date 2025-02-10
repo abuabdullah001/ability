e79c4f22af675d0dc54b5f132a38d4504a5882d5
Sadman Sakib <saadman_0000@gmail.com>
2025-02-10 15:36:31 +0600

    about-index

--- resources/views/admin/pages/aboutmenu/indexabout.blade.php
+++ resources/views/admin/pages/aboutmenu/indexabout.blade.php
@@ -38,7 +38,7 @@ ALL GALLERY IMAGES
                             <h3 class="card-title"> <i class="fa fa-users"></i> Aboute page</h3>
                         </div>
                         <!-- /.card-header -->
-                        <div class="card-body">
+                        <div class="card-body table-responsive">
                             <table id="example1" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
